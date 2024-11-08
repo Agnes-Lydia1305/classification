3467f7934c6fbe8f2b19c42ae99882e42ed88b9b
Agnes-Lydia1305 <98098335+Agnes-Lydia1305@users.noreply.github.com>
2024-11-08 17:27:00 +0530

    Update Classification_Stream2.py

--- Classification_Stream2.py
+++ Classification_Stream2.py
@@ -1,64 +1,31 @@
-"""Create an Image Classification Web App using PyTorch and Streamlit."""
-# import libraries
-from PIL import Image
-from torchvision import models, transforms
-import torch
 import streamlit as st
+import numpy as np
+from skimage.io import imread
+from skimage.transform import resize
+import pickle 
+from PIL import Image
+st.set_option('deprecation.showfileUploaderEncoding', False)
+st.title('Image Classifier using Machine Learning')
+st.text('Upload the Image from the listed category.\n[Rose, Cricket Bat, Icecream Cone, Covid Vaccine, Chocolate]')
 
-# set title of app
-st.title("Simple Image Classification Application")
-st.write("")
+model = pickle.load(open('img_model.p','rb'))
 
-# enable users to upload images for the model to make predictions
-file_up = st.file_uploader("Upload an image", type = "jpg")
+uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+if uploaded_file is not None:
+  img = Image.open(uploaded_file)
+  st.image(img,caption='Uploaded Image')
 
-
-def predict(image):
-    """Return top 5 predictions ranked by highest probability.
-
-    Parameters
-    ----------
-    :param image: uploaded image
-    :type image: jpg
-    :rtype: list
-    :return: top 5 predictions ranked by highest probability
-    """
-    # create a ResNet model
-    resnet = models.resnet101(pretrained = True)
-
-    # transform the input image through resizing, normalization
-    transform = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(
-            mean = [0.485, 0.456, 0.406],
-            std = [0.229, 0.224, 0.225]
-            )])
-
-    # load the image, pre-process it, and make predictions
-    img = Image.open(image)
-    batch_t = torch.unsqueeze(transform(img), 0)
-    resnet.eval()
-    out = resnet(batch_t)
-
-    with open('imagenet_classes.txt') as f:
-        classes = [line.strip() for line in f.readlines()]
-
-    # return the top 5 predictions ranked by highest probabilities
-    prob = torch.nn.functional.softmax(out, dim = 1)[0] * 100
-    _, indices = torch.sort(out, descending = True)
-    return [(classes[idx], prob[idx].item()) for idx in indices[0][:5]]
-
-
-if file_up is not None:
-    # display image that user uploaded
-    image = Image.open(file_up)
-    st.image(image, caption = 'Uploaded Image.', use_column_width = True)
-    st.write("")
-    st.write("Just a second ...")
-    labels = predict(file_up)
-
-    # print out the top 5 prediction labels with scores
-    for i in labels:
-        st.write("Prediction (index, name)", i[0], ",   Score: ", i[1])
+  if st.button('PREDICT'):
+    Categories = ['rose','cricket bat','icecream cone','covid vaccine','chocolate']    
+    st.write('Result...')
+    flat_data=[]
+    img = np.array(img)
+    img_resized = resize(img,(150,150,3))
+    flat_data.append(img_resized.flatten())
+    flat_data = np.array(flat_data)
+    y_out = model.predict(flat_data)
+    y_out = Categories[y_out[0]]
+    st.title(f' PREDICTED OUTPUT: {y_out.upper()}')
+    q = model.predict_proba(flat_data)
+    for index, item in enumerate(Categories):
+      st.write(f'{item} : {q[0][index]*100}%')
